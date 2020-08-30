0c2d998b0d813798bbe84be1be9aa6c17e8983b0
AleksandrBracyuk <aleksandr.bracyuk@abcsolutions.pro>
2020-08-30 21:40:52 +0300

    изобретаю функцию состояния (простую фугнкцию :) )

--- app/src/app/timer2/timer2.component.ts
+++ app/src/app/timer2/timer2.component.ts
@@ -44,6 +44,9 @@ interface Time2StateCommand {
   isStarted: boolean;
   isWaited: boolean;
   command: Timer2ClickButton;
+  commandDate: number;
+  startDate: number;
+  waitDate: number;
   currentSecond: number;
 }
 
@@ -58,12 +61,56 @@ export class Timer2Component implements OnInit, AfterViewInit {
   @ViewChild('resetButton') resetButton: ElementRef;
 
   data: Observable<Date>;
-  secondsAfterStart: number = 0;
 
   constructor() {}
 
   ngOnInit(): void {
     // this.data = NEVER.pipe(startWith(new Date(2020, 0, 1, 0, 0, 0)));
+  }
+
+  private NextState(
+    oldState: Time2StateCommand,
+    command: Timer2ClickButton,
+    commandDate: number
+  ): Time2StateCommand {
+    let ret = { ...oldState, ...{ command, commandDate } };
+    let stateStart = oldState.isStarted && !oldState.isWaited;
+    let stateStop = !oldState.isStarted;
+    let stateWait = oldState.isStarted && oldState.isWaited;
+
+    if (command == Timer2ClickButton.startButton) {
+      ret.isStarted = stateStop
+        ? true
+        : stateStart
+        ? false
+        : oldState.isStarted;
+      ret.isWaited = false;
+      ret.command = stateStart ? Timer2ClickButton.stopButton : command;
+      ret.startDate = stateWait
+        ? commandDate - (oldState.waitDate - oldState.startDate)
+        : /* он простоял ранее интервал (oldState.waitDate - oldState.startDate)
+        поэтому тепеть он стартует конечно в commandDate, но чтобы счетчик учел время простоя - то время старта сдвигаем
+        в прошлое на время простоя, т.е. на  (oldState.waitDate - oldState.startDate)*/
+        stateStop
+        ? commandDate
+        : stateStart
+        ? 0
+        : ret.startDate;
+      ret.waitDate = stateStop ? 0 : stateStart ? 0 : ret.waitDate;
+    } else if (command == Timer2ClickButton.waitButton) {
+      ret.isWaited = true;
+      ret.waitDate = stateStart ? commandDate : ret.waitDate;
+    } else if (command == Timer2ClickButton.resetButton) {
+      ret.isWaited = true;
+      ret.startDate = stateStart ? commandDate : 0;
+      ret.waitDate = 0;
+    } else if (command == Timer2ClickButton.stopButton) {
+      ret.isWaited = true;
+      ret.startDate = 0;
+      ret.waitDate = 0;
+    }
+
+    return ret;
   }
 
   ngAfterViewInit() {
@@ -83,51 +130,29 @@ export class Timer2Component implements OnInit, AfterViewInit {
       stream(this.resetButton.nativeElement, Timer2ClickButton.resetButton)
     );
 
+    let startItem = {
+      isStarted: false,
+      isWaited: false,
+      command: Timer2ClickButton.stopButton,
+      commandDate: 0,
+      startDate: 0,
+      waitDate: 0,
+      currentSecond: 0,
+    };
     let events$ = eventsRaw$.pipe(
       map((x) => ({
         isStarted: false,
         isWaited: false,
         command: x,
+        commandDate: Date.now(),
+        startDate: 0,
+        waitDate: 0,
         currentSecond: 0,
       })),
       scan(
-        (s: Time2StateCommand, curr) => ({
-          ...s,
-          ...{
-            isStarted:
-              curr.command == Timer2ClickButton.startButton && !s.isStarted
-                ? true
-                : curr.command == Timer2ClickButton.startButton &&
-                  s.isStarted &&
-                  !s.isWaited
-                ? false
-                : s.isStarted,
-          },
-          ...{
-            isWaited:
-              curr.command == Timer2ClickButton.waitButton
-                ? true
-                : curr.command == Timer2ClickButton.startButton
-                ? false
-                : s.isWaited,
-          },
-          ...{
-            command:
-              s.command == Timer2ClickButton.stopButton
-                ? curr.command
-                : curr.command == Timer2ClickButton.startButton &&
-                  s.isStarted &&
-                  !s.isWaited
-                ? Timer2ClickButton.stopButton
-                : curr.command,
-          },
-        }),
-        {
-          isStarted: false,
-          isWaited: false,
-          command: Timer2ClickButton.stopButton,
-          currentSecond: 0,
-        }
+        (s: Time2StateCommand, curr) =>
+          this.NextState(s, curr.command, curr.commandDate),
+        startItem
       ),
       publish(),
       refCount()
@@ -142,9 +167,8 @@ export class Timer2Component implements OnInit, AfterViewInit {
       }),
       switchMap((e) => {
         if (e.isStarted && !e.isWaited) {
-          let startValue = e.currentSecond;
           return interval(1000).pipe(
-            map((x) => ({ ...e, ...{ currentSecond: startValue + x } }))
+            map((x) => ({ ...e, ...{ currentSecond: x } }))
           );
         } else {
           return NEVER.pipe(startWith(e));
@@ -155,7 +179,6 @@ export class Timer2Component implements OnInit, AfterViewInit {
     );
 
     let data$ = super$.pipe(
-      // tap((x) => (this.secondsAfterStart = x.currentSecond)),
       map((x) => new Date(2020, 0, 1, 0, 0, x.currentSecond))
     );
     this.data = data$;
